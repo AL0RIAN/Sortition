b6a9de582d15db856a5045361e30357b7c35b2c4
MEGORD <megordzp@gmail.com>
2023-09-10 02:36:38 +0300

    Compare first and fourth element.

--- new GUI/main_window.py
+++ new GUI/main_window.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from sorting.tournaments_list import PAIR_LIST
 from sorting.tournaments_list import TREE
 
+gender_flag = ''
+age_flag = ''
+weight_flag = ''
+
 
 '''
 У нас будет 4 элемента:
@@ -49,15 +53,21 @@ First Element, with functions of switching in OptionMenu.
 
 def man_checkbutton(*args):
     if man_choices.get() == 1:
+        global gender_flag
+        gender_flag = 'М'
         woman_choices.set(0)
         new_choices = list(choices_dict['М'])
         age_choice_menu.set(new_choices[0])
         age_option_menu['menu'].delete(0, 'end')
+
         for choice in new_choices:
             age_option_menu['menu'].add_command(label=choice,
                                                 command=lambda new_choice=choice: man_age_checkbutton(new_choice))
 
+
 def man_age_checkbutton(choice):
+    global age_flag
+    age_flag = age_choice_menu.get()
     new_choice = choice
     age_choice_menu.set(new_choice)
     new_choices = list(choices_dict['М'][age_choice_menu.get()])
@@ -71,15 +81,20 @@ def man_age_checkbutton(choice):
 
 def woman_checkbutton(*args):
     if woman_choices.get() == 1:
+        global gender_flag
+        gender_flag = 'Ж'
         man_choices.set(0)
         new_choices = list(choices_dict['Ж'])
         age_choice_menu.set(new_choices[0])
         age_option_menu['menu'].delete(0, 'end')
+
         for choice in new_choices:
             age_option_menu['menu'].add_command(label=choice,
                                                 command=lambda new_choice=choice: woman_age_checkbutton(new_choice))
 
 def woman_age_checkbutton(choice):
+    global age_flag
+    age_flag = age_choice_menu.get()
     new_choice = choice
     age_choice_menu.set(new_choice)
     new_choices = list(choices_dict['Ж'][age_choice_menu.get()])
@@ -89,6 +104,13 @@ def woman_age_checkbutton(choice):
     for choice in new_choices:
         weight_option_menu['menu'].add_command(label=choice,
                                                command=lambda new_choice=choice: weight_choice_menu.set(new_choice))
+
+
+def get_selected_value():
+    global weight_flag
+    weight_flag = weight_choice_menu.get()
+    get_tree(gender_flag, age_flag, weight_flag)
+
 
 
 # def woman_checkbutton():
@@ -117,11 +139,12 @@ def woman_age_checkbutton(choice):
 #                                                         new_choice))
 
 
-first_element.grid_rowconfigure(0, minsize=60)
-first_element.grid_rowconfigure(1, minsize=60)
-first_element.grid_rowconfigure(2, minsize=60)
+first_element.grid_rowconfigure(0, minsize=40)
+first_element.grid_rowconfigure(1, minsize=50)
+first_element.grid_rowconfigure(2, minsize=50)
 first_element.grid_columnconfigure(0, minsize=125)
 first_element.grid_columnconfigure(1, minsize=125)
+first_element.grid_rowconfigure(3, minsize=60)
 
 woman_choices = tk.IntVar()
 man_choices = tk.IntVar()
@@ -136,6 +159,7 @@ age_option_menu = tk.OptionMenu(first_element, age_choice_menu, *age_choices)
 age_option_describe = tk.Label(first_element, bg='#a9c799', text='Віков група')
 weight_option_menu = tk.OptionMenu(first_element, weight_choice_menu, *weight_choices)
 weight_option_describe = tk.Label(first_element, bg='#a9c799', text='Вісова група')
+next_tree = tk.Button(first_element, text='Наступне дерево', command=get_selected_value)
 
 
 man_button.grid(row=0, column=0)
@@ -144,6 +168,7 @@ age_option_menu.grid(row=1, column=0)
 age_option_describe.grid(row=1, column=1, sticky='wens')
 weight_option_menu.grid(row=2, column=0)
 weight_option_describe.grid(row=2, column=1, sticky='wens')
+next_tree.grid(row=3, sticky='wens', columnspan=2)
 
 
 '''
@@ -151,6 +176,7 @@ Second Element.
 '''
 
 count = 0
+
 
 def next_battle():
     global count
@@ -202,61 +228,63 @@ Usual tree
 '''
 
 
-for i in range(16):
-    if i % 2 == 0:
-        flag = 0
-    else:
-        flag = 1
-    fourth_element.grid_rowconfigure(i, minsize=33)
-    fourth_element.grid_columnconfigure(i, minsize=136)
-    try:
-        athlete = tk.Label(fourth_element, text=TREE[0]['М']['18+']['48-'][i//2][flag]["name"], bg="#DCF4FF")
-    except TypeError:
-        athlete = tk.Label(fourth_element, text=TREE[0]['М']['18+']['48-'][i//2][flag], bg="#DCF4FF")
-    athlete.grid(column=0, row=i)
-'''
-2 step
-'''
-for i in range(8):
-    if i % 2 == 0:
-        flag = 0
-    else:
-        flag = 1
-    try:
-        athlete = tk.Label(fourth_element, text=TREE[1]['М']['18+']['48-'][i//2][flag]["name"], bg="#DCF4FF")
-    except TypeError:
-        athlete = tk.Label(fourth_element, text=TREE[1]['М']['18+']['48-'][i//2][flag], bg="#DCF4FF")
-    athlete.grid(column=1, row=2 * i, rowspan=2)
-'''
-3 step
-'''
-for i in range(4):
-    if i % 2 == 0:
-        flag = 0
-    else:
-        flag = 1
-    try:
-        athlete = tk.Label(fourth_element, text=TREE[2]['М']['18+']['48-'][i//2][flag]["name"], bg="#DCF4FF")
-    except TypeError:
-        athlete = tk.Label(fourth_element, text=TREE[2]['М']['18+']['48-'][i//2][flag], bg="#DCF4FF")
-    athlete.grid(column=2, row=4 * i, rowspan=4)
-'''
-4 step
-'''
-for i in range(2):
-    if i % 2 == 0:
-        flag = 0
-    else:
-        flag = 1
-    try:
-        athlete = tk.Label(fourth_element, text=TREE[3]['М']['18+']['48-'][i//2][flag]["name"], bg="#DCF4FF")
-    except TypeError:
-        athlete = tk.Label(fourth_element, text=TREE[3]['М']['18+']['48-'][i//2][flag], bg="#DCF4FF")
-    athlete.grid(column=3, row=8 * i, rowspan=8)
+def get_tree(gender, age, weight):
+    for i in range(16):
+        if i % 2 == 0:
+            flag = 0
+        else:
+            flag = 1
+        fourth_element.grid_rowconfigure(i, minsize=33)
+        fourth_element.grid_columnconfigure(i, minsize=136)
+        try:
+            athlete = tk.Label(fourth_element, text=TREE[0][gender][age][weight][i//2][flag]["name"], bg="#DCF4FF")
+        except TypeError:
+            athlete = tk.Label(fourth_element, text=TREE[0][gender][age][weight][i//2][flag], bg="#DCF4FF")
+        athlete.grid(column=0, row=i)
+    '''
+    2 step
+    '''
+    for i in range(8):
+        if i % 2 == 0:
+            flag = 0
+        else:
+            flag = 1
+        try:
+            athlete = tk.Label(fourth_element, text=TREE[1][gender][age][weight][i//2][flag]["name"], bg="#DCF4FF")
+        except TypeError:
+            athlete = tk.Label(fourth_element, text=TREE[1][gender][age][weight][i//2][flag], bg="#DCF4FF")
+        athlete.grid(column=1, row=2 * i, rowspan=2)
+    '''
+    3 step
+    '''
+    for i in range(4):
+        if i % 2 == 0:
+            flag = 0
+        else:
+            flag = 1
+        try:
+            athlete = tk.Label(fourth_element, text=TREE[2][gender][age][weight][i//2][flag]["name"], bg="#DCF4FF")
+        except TypeError:
+            athlete = tk.Label(fourth_element, text=TREE[2][gender][age][weight][i//2][flag], bg="#DCF4FF")
+        athlete.grid(column=2, row=4 * i, rowspan=4)
+    '''
+    4 step
+    '''
+    for i in range(2):
+        if i % 2 == 0:
+            flag = 0
+        else:
+            flag = 1
+        try:
+            athlete = tk.Label(fourth_element, text=TREE[3][gender][age][weight][i//2][flag]["name"], bg="#DCF4FF")
+        except TypeError:
+            athlete = tk.Label(fourth_element, text=TREE[3][gender][age][weight][i//2][flag], bg="#DCF4FF")
+        athlete.grid(column=3, row=8 * i, rowspan=8)
 
 '''
 Troika
 '''
+
 
 lottery.mainloop()
 
